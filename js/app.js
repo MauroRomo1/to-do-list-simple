175e4087f13ef08b0a7cf094779eb61151b5d882
Mauro Romo <mauroromo.developer@outlook.com>
2023-06-08 19:10:14 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let card = /* HTML */ `
+  <div class="card my-3">
+    <div class="card-body d-flex justify-content-between">
+      <p>This is some text within a card body.</p>
+      <div class="d-grid gap-2 d-md-block">
+        <button class="btn btn-primary">
+          <i class="fa-regular fa-square-check"></i>
+        </button>
+        <button class="btn btn-danger">
+          <i class="fa-solid fa-trash-can"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+`;
